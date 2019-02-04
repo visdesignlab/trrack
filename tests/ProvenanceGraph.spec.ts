05adf6e8336458511b1d966102b1d368cd1d110a
Kiran Gadhave <kirangadhave2@gmail.com>
2019-02-03 18:33:39 -0700

    -

--- tests/ProvenanceGraph.spec.ts
+++ tests/ProvenanceGraph.spec.ts
@@ -3,51 +3,14 @@ import {
   ActionTemplate,
   RegisterActionTemplate
 } from "../src/provenance-core/Actions";
-let a = Provenance();
 
-let b = a.graph();
+import "mocha";
+import "assert";
+import { expect } from "chai";
 
-class Counter {
-  count: number = 0;
-
-  add() {
-    this.count++;
-  }
-
-  sub() {
-    this.count--;
-  }
-
-  print() {
-    console.log(this.count);
-  }
-}
-
-let ct = new Counter();
-ct.print();
-
-let addAction: RegisterActionTemplate = {
-  name: "add",
-  do: {
-    func: ct.add,
-    thisArgs: ct
-  },
-  undo: {
-    func: ct.sub,
-    thisArgs: ct
-  }
-};
-
-a.register(addAction);
-
-a.apply({
-  name: "add",
-  doArgs: [],
-  undoArgs: []
-}).then(done => {
-  ct.print();
-
-  a.goTo(a.graph().root.id).then(done => {
-    ct.print();
+describe("Hello Function", () => {
+  it("should return hello world", () => {
+    const result = "hello world";
+    expect(result).to.equal("hello world");
   });
 });
