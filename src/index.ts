b5ec40836883dfe881c3864483a5948938189ba1
Kiran Gadhave <kirangadhave2@gmail.com>
2019-02-05 14:46:34 -0700

    Added export for SerializedProvenanceGraph

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export { Provenance } from "./provenance";
 export { RegisterActionTemplate, ApplyAction } from "./provenance-core/Actions";
+export {
+  SerializedProveananceGraph
+} from "./provenance-core/SerializedInterfaces";
