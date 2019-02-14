a438b39079f3b602844d0bf10cbf95f777c6fc09
Kiran Gadhave <kirangadhave2@gmail.com>
2019-02-13 19:27:33 -0700

    Added proper exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
 export { Provenance } from "./provenance-core/ProvenanceCore";
+export {
+  StateNode,
+  ProvenanceNode,
+  RootNode
+} from "./provenance-core/NodeInterfaces";
+
+export {
+  ReversibleActionCreator,
+  ReversibleAction,
+  DoAction,
+  UndoAction,
+  GenericAction
+} from "./provenance-core/ProvenanceActions";
