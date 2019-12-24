1fad9c5d065c28b5ab1aad281e85e2f8530ce392
Kiran Gadhave <kirangadhave2@gmail.com>
2019-12-23 21:16:14 -0700

    feat(index.ts): exporting initialize function
    BREAKING CHANGE: exporting initialize, instead of nothing

--- src/index.ts
+++ src/index.ts
@@ -2,36 +2,6 @@
 // import "core-js/fn/array.find"
 // ...
 
-import produce from 'immer';
+import initProvenance from './Provenance/InitializeProvenance';
 
-export interface State {
-  count: number;
-  message: string;
-  complex: {
-    count: number;
-    payload: {
-      arg1: string;
-      arg2: any;
-    };
-  };
-}
-
-export default function doSomething() {
-  const baseState: State = {
-    count: 0,
-    message: 'Initial',
-    complex: {
-      count: -1,
-      payload: {
-        arg1: 'Inital payload arg1',
-        arg2: {
-          message: 'I can be anything'
-        }
-      }
-    }
-  };
-
-  const nextState = produce(baseState, draftState => {
-    draftState.count = 2;
-  });
-}
+export { initProvenance };
