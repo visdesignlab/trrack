987962991bacf1d2affefc836b84277e8be7f59b
Kiran Gadhave <kirangadhave2@gmail.com>
2019-12-24 17:41:23 -0700

    test(.spec.ts): adding tests for initialize provenance

--- test/ProvenanceGraph/Provenance.spec.ts
+++ test/ProvenanceGraph/Provenance.spec.ts
@@ -1,73 +1,105 @@
 import initProvenance from '../../src/Provenance/InitializeProvenance';
+import { ProvenanceGraph } from '../../src/Interfaces/ProvenanceGraph';
 import { isStateNode } from '../../src/Interfaces/NodeInterfaces';
 
-describe('Initiailize Provenance Graph', () => {
-  interface State {
-    count: number;
-    message: {
-      hi: string;
-    };
-  }
+enum TodoStatus {
+  DONE = 'DONE',
+  ONGOING = 'ONGOING',
+  FUTURE = 'FUTURE'
+}
 
-  const initialState: State = {
-    count: 0,
-    message: {
-      hi: 'how are you?'
+interface TodoItem {
+  task: string;
+  createdOn: string;
+  status: TodoStatus;
+  completedOn: string;
+}
+
+type Todos = TodoItem[];
+
+interface ToDoListState {
+  todos: Todos;
+}
+
+const state: ToDoListState = {
+  todos: [
+    {
+      task: 'Add unit tests',
+      createdOn: new Date().toISOString(),
+      status: TodoStatus.ONGOING,
+      completedOn: ''
     }
-  };
+  ]
+};
 
-  const provenance = initProvenance(initialState, true);
+const provenance = initProvenance(state, false);
 
-  const actionFunction = (state: State, arg1: any, arg2: any) => {
-    state.count += arg1;
-    return state;
-  };
+describe('Initialization', () => {
+  it('provenance is a valid Provenance object', () => {
+    const functionProperties = [
+      'graph',
+      'current',
+      'root',
+      'applyAction',
+      'addObserver',
+      'addGlobalObserver',
+      'goToNode',
+      'goBackNSteps',
+      'goBackOneStep',
+      'goForwardOneStep',
+      'reset',
+      'done',
+      'exportState',
+      'importState',
+      'exportProvenanceGraph',
+      'importProvenanceGraph'
+    ];
 
-  const actionFunction2 = (state: State, arg1: any) => {
-    state.message.hi = 'Changed';
-    return state;
-  };
+    functionProperties.forEach(property => {
+      expect(provenance).toHaveProperty(property);
+    });
+  });
+});
 
-  provenance.addObserver(['message'], (state?: State) => {
-    console.log('Yay Partial');
+describe('provenance.graph returns a valid provenance object', () => {
+  it('has valid keys', () => {
+    expect(provenance.graph()).toMatchSnapshot({
+      current: expect.any(String),
+      root: expect.any(String),
+      nodes: expect.any(Object)
+    });
   });
 
-  provenance.addObserver(['message', 'hi'], (state?: State) => {
-    console.log('Yay');
+  const { nodes, current, root } = provenance.graph();
+
+  it('has default task added', () => {
+    expect(Object.keys(nodes).length).toBe(1);
+
+    const defaultTaskId = Object.keys(nodes)[0];
+
+    expect(nodes[defaultTaskId].state.todos.length).toBe(1);
   });
 
-  provenance.addObserver(['count'], (state?: State) => {
-    console.log('Hello', state);
-  });
+  test('default task matches the snapshot', () => {
+    const defaultTaskId = Object.keys(nodes)[0];
 
-  provenance.applyAction('Add 2 to count', actionFunction, [2]);
+    const defaultTask = nodes[defaultTaskId].state.todos[0];
 
-  provenance.goBackOneStep();
-
-  provenance.applyAction('Add 3 to count', actionFunction, [3]);
-  // console.log(JSON.stringify(provenance.current(), null, 4));
-
-  provenance.goBackOneStep();
-  // console.log(JSON.stringify(provenance.current(), null, 4));
-
-  provenance.applyAction('Changed message', actionFunction2);
-  provenance.goBackOneStep();
-
-  provenance.goForwardOneStep();
-
-  const exportedState = provenance.exportState();
-
-  const windowLoc = JSON.stringify(window.location);
-  delete window.location;
-  Object.defineProperty(window, 'location', {
-    value: JSON.parse(windowLoc)
-  });
-
-  window.location.href += exportedState;
-
-  provenance.done();
-
-  test('Hello', () => {
-    expect(true).toBeTruthy();
+    expect(defaultTask).toMatchInlineSnapshot(
+      {
+        task: 'Add unit tests',
+        createdOn: expect.any(String),
+        status: TodoStatus.ONGOING,
+        completedOn: expect.any(String)
+      },
+      `
+      Object {
+        "completedOn": Any<String>,
+        "createdOn": Any<String>,
+        "status": "ONGOING",
+        "task": "Add unit tests",
+      }
+    `
+    );
   });
 });
