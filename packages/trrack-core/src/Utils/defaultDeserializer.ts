48cead87e58e5ff7aec198cf569cc50cc0a9ef6f
Kiran Gadhave <kirangadhave2@gmail.com>
2021-05-07 13:50:24 -0600

    fix: defaultDeserialzer

--- packages/trrack-core/src/Utils/defaultDeserializer.ts
+++ packages/trrack-core/src/Utils/defaultDeserializer.ts
@@ -3,6 +3,8 @@ import { JsonValue } from '../Types/Serializers';
 function defaultDeserializer<T>(o: JsonValue): T {
   const str = JSON.stringify(o);
   const obj = JSON.parse(str, (_, val) => {
+    if (!val) return val;
+
     if (val.type && val.type === 'Set') {
       return new Set(val.arr);
     }
